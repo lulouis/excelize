69956be128f7a75d7a6d2237c567ab6163e1afa5
louis <zhuaiman@hotmail.com>
2025-04-07 14:24:47 +0800

    引用依赖本地化

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/360EntSecGroup-Skylar/excelize/v2
+module github.com/lulouis/excelize
 
 go 1.12
 
--- sheet_test.go
+++ sheet_test.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/360EntSecGroup-Skylar/excelize/v2"
+	"github.com/lulouis/excelize"
 
 	"github.com/mohae/deepcopy"
 	"github.com/stretchr/testify/assert"
